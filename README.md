d43bd186a622809bf2b5e61625ab7695de439580
Revanshu Pusadkar <revanshu444@gmail.com>
2025-06-30 23:31:17 +0530

    connect to mongo atlas

---
+++ README.md
@@ -0,0 +1,6 @@
+Notes:
+
+1. Watch the folder structure, this is how the production level code is written.
+2. Install Prettier package locally so that while collaborating with othere it gets easier to maintain the code structure.
+3. Store the secret info which you don't want to compramise in dotenv file
+4. There are to approaches to connect database (Check the db folder and index.js)
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./env"
+});
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+connectDB()
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+
+/* First Approach
+
+import express from "express";
+const app = express();
+
+(async () => {
+    try{
+        await mongoose.connect(`
+            ${process.env.MONGO_URI}/
+            ${DB_NAME}`,)
+            app.on('error', (error) => {
+                console.error('MongoDB connection error:', error);
+            });
+
+            app.listen(process.env.PORT || 3000, () => {
+                console.log(`Server is running on port ${process.env.PORT || 3000}`);
+            });
+        console.log('Connected to MongoDB');
+
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+})()
+
+*/
